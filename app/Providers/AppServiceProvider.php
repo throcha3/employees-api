ad87956fda5b0b4ea22936ed9ff75696d182a52d
throcha3 <throcha3@gmail.com>
2025-10-20 14:20:21 -0300

    chore: install horizon and create container to run queues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Horizon\Horizon;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Horizon authorization
+        Horizon::auth(function ($request) {
+            return Gate::allows('viewHorizon', [$request->user()]);
+        });
+
+        Gate::define('viewHorizon', function ($user) {
+            if (!$user) {
+                return false;
+            }
+            return method_exists($user, 'isAdmin') ? $user->isAdmin() : true; // ajuste conforme sua regra
+        });
     }
 }
